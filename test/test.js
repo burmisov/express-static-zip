e5f4a9e76c71e49cf03f409f54cd61c91d2d81a2
burmisov <burmisov@informatica.ru>
2014-01-19 02:39:13 +0400

    setup some basic tests

--- test/test.js
+++ test/test.js
@@ -1,18 +1,53 @@
-var testZipPath = './testdata.zip';
-var testZipReferencePath = './testdata/';
+var testZipPath = 'testdata.zip';
+var testZipReferencePath = 'testdata/';
 
 var fs = require('express');
+var path = require('path');
 var express = require('express');
 var request = require('supertest');
 var staticZip = require('../index.js');
 
 var app = express();
 
-app.use(staticZip(__dirname + '/testdata.zip'));
+var otherUrlPath = '/some/other/lib/path';
 
-request(app)
-	.get('/some-file.txt')
-	.expect(200)
-	.end(function (err, res) {
-		if (err) throw err;
+var staticZipRoot = staticZip(path.join(__dirname, testZipPath));
+
+app.use(staticZipRoot);
+app.use(otherUrlPath, staticZipRoot);
+
+describe('Serving files from root of zip on root url path', function () {
+	it('should correctly serve file matching zip content', function (done) {
+		request(app)
+			.get('/some-file.txt')
+			.expect(200)
+			.expect('Content-type','text/plain')
+			.expect('File 1 content')
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});	
 	});
+
+	it('should return 404 when file path does not match zip content', function (done) {
+		request(app)
+			.get('/some-missing-file.txt')
+			.expect(404)
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});	
+	});
+
+	it('should correctly serve file from zip subfolder', function (done) {
+		request(app)
+			.get('/a-folder/file-in-a-folder.txt')
+			.expect(200)
+			.expect('Content-type','text/plain')
+			.expect('File-in-a-folder content.')
+			.end(function (err, res) {
+				if (err) return done(err);
+				done();
+			});	
+	});
+});
