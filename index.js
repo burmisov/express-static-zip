e10192ae429d8def7feb568df73c6897edf819b6
Сергей Бурмисов <burmisov@informatica.ru>
2014-01-18 16:28:44 +0400

    add zip content reader

--- index.js
+++ index.js
@@ -6,6 +6,16 @@ var zip = require('zip');
 var StaticZip = exports.StaticZip = function (pathToZip) {
 	// Zip contents registry; keys are zip entry names
 	this.zipDir = {};
+
+	// Read zip contents and populate the registry
+	var data = fs.readFileSync(pathToZip);
+	var reader = zip.Reader(data);
+	reader.forEach(function (entry) {
+		// Only take file entries (ignore directories)
+		if (entry.isFile()) {
+			this.zipDir[entry.getName()] = entry;	
+		}		
+	});	
 };
 
 // The middleware itself
